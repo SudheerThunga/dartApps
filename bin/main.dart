167ec1cbd22171ec5edd21add49668b17305d7c5
Sudheer <sudheerthunga@saimail.com>
2019-08-11 23:02:48 +0530

    Sample project

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:Sample_App/Sample_App.dart' as sample_app;
+
+main(List<String> arguments) {
+  print('Hello world: ${sample_app.calculate()}!');
+}
---
+++ test/Sample_App_test.dart
@@ -0,0 +1,8 @@
+import 'package:Sample_App/Sample_App.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
